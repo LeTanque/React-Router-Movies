368e42b6180205a3d9de0b75e7683e2e0eb57be9
Frank Martinez <frankie.martinez@gmail.com>
2019-02-26 15:55:08 -0800

    really messy MVP

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,5 +1,9 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import { Link } from 'react-router-dom';
 import axios from 'axios';
+
+// import MovieCard from './MovieCard';
+
 
 export default class MovieList extends Component {
   constructor(props) {
@@ -9,6 +13,9 @@ export default class MovieList extends Component {
     };
   }
 
+
+  // Provide axios code //////////////////////////////////
+  // Communicated with server, provides error notification
   componentDidMount() {
     axios
       .get('http://localhost:5000/api/movies')
@@ -19,36 +26,62 @@ export default class MovieList extends Component {
         console.error('Server Error', error);
       });
   }
+  // Provide axios code //////////////////////////////////
+  ////////////////////////////////////////////////////////
+
 
   render() {
+    // console.log(this.state);
     return (
-      <div className="movie-list">
-        {this.state.movies.map(movie => (
-          <MovieDetails key={movie.id} movie={movie} />
-        ))}
-      </div>
+      <Fragment>
+        <div className="movie-list">
+          {/* Iterate over entire movie list, grab each movie and return array */}
+          {this.state.movies.map(movie => {
+            return (
+              <Fragment key={movie.id} >
+                {/* <MovieCard 
+                  movie={movie} 
+                /> */}
+                <MovieDetails 
+                  movie={movie} 
+                />
+              </Fragment>
+            )
+          }
+          )}
+        </div>
+        
+      </Fragment>
     );
   }
 }
 
 function MovieDetails({ movie }) {
-  const { title, director, metascore, stars } = movie;
+  const { title, director, metascore, stars, id } = movie;
   return (
-    <div className="movie-card">
-      <h2>{title}</h2>
-      <div className="movie-director">
-        Director: <em>{director}</em>
-      </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
-      <h3>Actors</h3>
+    <Fragment>
 
-      {stars.map(star => (
-        <div key={star} className="movie-star">
-          {star}
+      <Link to={`/movies/${id}`}>
+        <div className="movie-card">
+          <h2>{title}</h2>
+          <div className="movie-director">
+            Director: <em>{director}</em>
+          </div>
+          <div className="movie-metascore">
+            Metascore: <strong>{metascore}</strong>
+          </div>
+          <h3>Actors</h3>
+
+          {/* Iterate over array of stars from within each movie and grab each name */}
+          {stars.map(star => (
+            <div key={star} className="movie-star">
+              {star}
+            </div>
+          ))}
+
         </div>
-      ))}
-    </div>
+      </Link>
+      
+    </Fragment>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './css/index.css';
 import App from './App';
 
-ReactDOM.render(
-    <Router>
+ReactDOM.render((
+    <BrowserRouter>
         <App />
-    </Router>, 
-    document.getElementById('root')
-);
+    </BrowserRouter>
+), document.getElementById('root'))
